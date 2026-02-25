37b51b725166df039f9671446ccf3161dd84ac03
ayushman-rabbit <ayushman.tripathi@rabbitinvest.com>
2026-02-25 15:23:24 +0530

    Updated Header component logo styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,10 +7,7 @@ export default function Header() {
       <div className="logo-section">
         <img src={logo} alt="NSE Logo" style={{ height: '40px', objectFit: 'contain' }} />
       </div>
-      <div className="nav-buttons">
-        <button className="btn-login">Login</button>
-        <button className="btn-signup">Sign up</button>
-      </div>
+      
     </header>
   );
 }
